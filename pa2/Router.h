b3f49950d5d6dd7958822c3c2048fe02563a33c1
kevin71104 <b03901026@ntu.edu.tw>
2016-12-08 00:37:22 +0800

    small structure

--- pa2/Router.h
+++ pa2/Router.h
@@ -3,11 +3,38 @@
 
 using namespace std;
 
+class Interval
+{
+public:
+    Interval(size_t start,size_t end):startTime(start), endTime(end){}
+    ~Interval(){}
+
+    //access functions
+    size_t getStart(){ return startTime;}
+    size_t getEnd(){ return endTime;}
+
+    //setting functions
+    addOverlap(size_t Id) {overlap.push_back(Id);}
+    addPriorNode(size_t Id) {priorNode.push_back(Id);}
+
+private:
+    size_t           intervalID;
+    size_t           trackNum;
+    size_t           startTime;
+    size_t           endTime;
+    vector<size_t>   overlap;
+    vector<size_t>   priorNode;
+}
+
 class Router
 {
 public:
-    
+    bool readNet(const string& filename);
+    void writeTrack(ostream& outfile);
 private:
+    size_t watermark;
+    vector<Interval*>       IntervalList;
+    vector<vector<int>>     trackList;
 }
 
 #endif
